031acc5ceab5bb3f9629bddb8be66ecc8e134a11
Timofeev Aleksandr <115435770+TimofeevAleksandrIVT32@users.noreply.github.com>
2023-10-09 06:02:49 +0300

    Update client.h

--- client.h
+++ client.h
@@ -6,4 +6,8 @@
 
 int client_socket(char *file_name);
 int read_config(char *file_name, char *soc);
+int settings_client_socket(char *soc);
+int write_data_soc(int data_socket, char *file_name);
+int read_data_soc(int data_socket);
+void print_result(char *buffer);
 #endif /* CLIENT_H */
