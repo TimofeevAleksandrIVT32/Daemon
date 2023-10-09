c817ec6716cc38482a4d0360d2baf67486b9b635
Timofeev Aleksandr <115435770+TimofeevAleksandrIVT32@users.noreply.github.com>
2023-10-09 06:04:52 +0300

    Update main_server.c

--- main_server.c
+++ main_server.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
+#include <string.h>
 #include "server.h"
 
 int main(int argc, char *argv[]) {
+    int log_flag = 1;
     if (argc > 2) {
         printf("Invalid number of arguments\n");
         return 1;
     }else if (argc == 2) {
-        if (argv[1][0] == 'd') {
+        if (!strcmp(argv[1],"-d")) {
+            log_flag = 0;
             if (daemonize()) {
+                finish_logging();
                 return 1;
             }
         }
@@ -16,7 +20,14 @@ int main(int argc, char *argv[]) {
             return 1;
         }
     }
-    if (server_socket()) {
+    if (log_flag) {
+        start_logging(argv[0]);
+    }
+    int er = server_socket();
+    if (log_flag) {
+        finish_logging();
+    }
+    if (er) {
         return 1;
     }
     return 0;
