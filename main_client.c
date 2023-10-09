78b37b95604125ca107c37872c53f759c4e3c083
Timofeev Aleksandr <115435770+TimofeevAleksandrIVT32@users.noreply.github.com>
2023-10-09 06:02:21 +0300

    Update main_client.c

--- main_client.c
+++ main_client.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
+#include <string.h>
 #include "client.h"
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
         printf("Invalid number of arguments\n");
+        return 1;
+    } else if ((strcmp(argv[1],"end")) && (strcmp(argv[1],"size"))) {
+        printf("Invalid argument\n");
         return 1;
     }
     if (client_socket(argv[1])) {
